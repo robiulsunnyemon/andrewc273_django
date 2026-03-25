79018637c2f7b612ab30bba75ae6400d57cf472c
Sakhawat Hossain <hsakhawat135@gmail.com>
2026-03-25 15:39:05 +0600

    stripe modification

--- apps/subscription/urls.py
+++ apps/subscription/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("subscription/upgrade/", UpgradeSubscriptionView.as_view()),
     path("subscription/my/", MySubscriptionView.as_view()),
     path("subscription/invoices/", InvoiceHistoryView.as_view()),
-    # path("webhook/stripe/", stripe_webhook),
+    path("webhook/stripe/", stripe_webhook),
     path("cards/save/", SaveCardView.as_view()),
     path("cards/list/", ListSavedCardsView.as_view()),
     path("payment/success/", StripePaymentSuccessView.as_view()),
