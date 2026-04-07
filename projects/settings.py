ab893d0d3551c384cc822f4861c22caec8562d9f
Sakhawat Hossain <hsakhawat135@gmail.com>
2026-04-07 11:30:34 +0600

    database update

--- projects/settings.py
+++ projects/settings.py
@@ -152,12 +152,10 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-SQLITE_PATH = os.getenv("SQLITE_PATH", str(BASE_DIR / "db.sqlite3"))
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': SQLITE_PATH,
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
