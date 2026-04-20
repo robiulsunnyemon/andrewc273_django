2dfafe7b4028cb5131e25dd9e3fe2a782aa0dfc9
AL Amin Islam <alamin88.ime@gmail.com>
2026-04-20 17:00:13 +0600

    add public status details

--- apps/smartcase/urls.py
+++ apps/smartcase/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-AcceptedCaseDetailAPIView, AuthorProfileView, CaseMediaSubmissionListAPIView, CaseSubmissionListCreateAPIView, CaseDetailAPIView, AIEnhanceTextView, AIAnalyzeLinkView, DownloadDocumentAPIView,UserCaseStatsView,AcceptedCaseListView
+AcceptedCaseDetailAPIView, AuthorProfileView, CaseMediaSubmissionListAPIView, CaseSubmissionListCreateAPIView, CaseDetailAPIView, AIEnhanceTextView, AIAnalyzeLinkView, DownloadDocumentAPIView,UserCaseStatsView,AcceptedCaseListView,PublicStatsView
 )
 urlpatterns = [
     path('cases/', CaseSubmissionListCreateAPIView.as_view(), name='case-list-create'),
@@ -14,4 +14,5 @@ urlpatterns = [
     path('cases/accepted/<int:pk>/', AcceptedCaseDetailAPIView.as_view(), name='accepted-case-detail'),
     # urls.py
     path('author-profile/<int:user_id>/', AuthorProfileView.as_view(), name='author-profile'),
+    path('public-stats/', PublicStatsView.as_view(), name='public-stats')
 ]
