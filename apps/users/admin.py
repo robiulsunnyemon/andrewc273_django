d05f113c4c85ddeca7db7b4df354e39941ccd94e
Sakhawat Hossain <hsakhawat135@gmail.com>
2026-04-25 10:39:09 +0600

    Refactor admin classes to use custom ModelAdmin and update settings for admin panel navigation

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -14,7 +14,7 @@ class CustomAdminClass(ModelAdmin):
 
 
 @admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
+class ProfileAdmin(ModelAdmin):
     list_display = ['user', 'name', 'has_podcast_story', 'total_posts', 'total_letters']
     list_editable = ['has_podcast_story'] # Make has_podcast_story editable in the list view
     search_fields = ['user__email', 'name']
